de9f144d644ed001ff24084f223033f356fd1fc2
Nenad Bogojevic <nbogojevic@hotmail.com>
2020-03-30 00:42:55 +0200

    Use -Update option

--- synology-freenom.sh
+++ synology-freenom.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
-docker run -i --rm nbogojevic/freenom-dynamic "$@"
+docker run -i --rm nbogojevic/freenom-dynamic $1 $2 $3 $4 -Update 
+
+# Uncomment to collect verbose logs
+# docker run -i --rm nbogojevic/freenom-dynamic $1 $2 $3 $4 -Update -Verbose >> /var/log/freenom.log 2>&1
