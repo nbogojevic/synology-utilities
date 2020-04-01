f90816536470c3a3a4fe3a003f9a3d884dd794b3
Nenad Bogojevic <nbogojevic@hotmail.com>
2020-04-01 15:58:20 +0200

    Add logging

--- synology-freenom.sh
+++ synology-freenom.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-docker run -i --rm nbogojevic/freenom-dynamic $1 $2 $3 $4 -Update 
+docker run -v /var/log:/var/log -i --rm nbogojevic/freenom-dynamic $1 $2 $3 $4 -Update -Log /var/log/synology-freenom.log
 
 # Uncomment to collect verbose logs
 # docker run -i --rm nbogojevic/freenom-dynamic $1 $2 $3 $4 -Update -Verbose >> /var/log/freenom.log 2>&1
